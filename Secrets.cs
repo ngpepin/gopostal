9c85599d9a4270f681f3b420bd14489d3cc1c52c
Nicolas Pepin <npepin@umiquity.com>
2024-09-25 18:31:48 -0400

    Update Secrets.cs

--- Secrets.cs
+++ Secrets.cs
@@ -1,12 +1,12 @@
 ﻿public static class Secrets
 {
-    public static readonly string ApiKey = "cd859ff5f9591fb4";
-    public static readonly string Secret = "897eb82fe9021caf68b563";
-    public static readonly string CustomerNumber = "0006123879";
+    public static readonly string ApiKey = "";
+    public static readonly string Secret = "";
+    public static readonly string CustomerNumber = "";
     public static readonly string Endpoint = "https://ct.soa-gw.canadapost.ca/rs/ship/price";
-    public static readonly string Street = "294 Besserer Street";
-    public static readonly string City = "Ottawa";
-    public static readonly string Province = "ON";
-    public static readonly string PostalCode = "K1N6B3";
+    public static readonly string Street = "";
+    public static readonly string City = "";
+    public static readonly string Province = "";
+    public static readonly string PostalCode = "";
 }
 
